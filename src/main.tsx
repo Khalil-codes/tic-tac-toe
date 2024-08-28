e2d84907cc2f31792e575ec31b03f7544e23bf3f
Khalil <khalilpati55@gmail.com>
2024-08-29 00:47:03 +0530

    Add relevant config and styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <main className="container mx-auto p-24">
+    <main className="container mx-auto flex min-h-screen max-w-lg flex-col items-center justify-center p-4">
       <App />
     </main>
   </StrictMode>
